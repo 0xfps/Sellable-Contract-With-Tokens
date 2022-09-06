bbf8656b34bbabc45113403cf81e52bd7e7d24b4
fps <anthony.nnaemeka.umeh@gmail.com>
2022-09-06 08:01:21 +0100

    Update Owned.sol

--- CoveredCall Deployed/contracts/Deps/Owned.sol
+++ CoveredCall Deployed/contracts/Deps/Owned.sol
@@ -2,63 +2,41 @@
 pragma solidity >=0.6.0;
 
 /*
-* @title: 
-* @author: Anthony (fps) https://github.com/fps8k .
+* @title: Owned contract.
+* @author: Anthony (fps) https://github.com/0xfps.
 * @dev: 
 */
-
-contract Owned
-{
+contract Owned {
     address internal contract_owner;
-
+    
     event MoveOwnership(address, address);
-
-
-
-
+   
     /*
     * @dev:
-    *
-    * Initializes the `contract_owner` to whoever deployed the contract.
-    */
-
-    constructor()
-    {
-        contract_owner = msg.sender;  
-        emit MoveOwnership(address(0), msg.sender);
-    }
-
-
-
-
-    /*
-    * @dev:
-    *
     * Validates that `msg.sender` is the contract owner.
     */
-
-    modifier isOwner()
-    {
+    modifier isOwner() {
         require(msg.sender == contract_owner, "!Owner");
         _;
     }
 
-
-
+    /*
+    * @dev:
+    * Initializes the `contract_owner` to whoever deployed the contract.
+    */
+    constructor() {
+        contract_owner = msg.sender;  
+        emit MoveOwnership(address(0), msg.sender);
+    }
 
     /*
     * @dev:
-    *
     * Moves ownership of conract from `contract_owner` to `new_contract_owner`.
     * 
-    *
     * @param:
-    * 
     * address new_contract_owner.
     */
-
-    function moveOwnership(address new_contract_owner) public isOwner
-    {
+    function moveOwnership(address new_contract_owner) public isOwner {
         require(new_contract_owner != address(0), "0 Address");
         contract_owner = new_contract_owner;
         emit MoveOwnership(msg.sender, new_contract_owner);
